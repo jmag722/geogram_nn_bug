909174337127b5a556e23546e41f76d1a0eb6e6c
Jared Magnusson <jmag722@gmail.com>
2024-11-26 07:02:02 -0600

    example added here showed C++ was giving me correct answer wrt scipy, but python wrapped was failing because of ownership issue

--- minimal_example.cpp
+++ minimal_example.cpp
@@ -1,25 +1,39 @@
 #include <vector>
+#include <span>
 #include <geogram/points/nn_search.h>
 
 int main(int argc, char const *argv[]) {
     std::vector<double> coords = {
-        // .1, .8, // without this line, cout outputs `9` below. with it I get `11`
-        0.63696169, 0.26978671,
-        0.04097352, 0.01652764,
-        0.81327024, 0.91275558,
-        0.60663578, 0.72949656,
-        0.54362499, 0.93507242,
+        0.84507479, 0.16097309,
+        0.55774455, 0.36807994,
+        0.21494196, 0.38582404,
+        0.4281645,  0.61134311
     };
-    // if I comment out Values (which isn't used) I get a different answer below (index 7!)
     std::vector<double> values = {
-        // 0.5,
-        0.81585355, 0.0027385, 0.85740428, 0.03358558, 0.72965545};
-    std::vector<double> query = {0.17565562, 0.86317892};
+        0.73638587, 0.01528966, 0.25404091, 0.60414585
+    };
+    std::vector<double> query = {
+        0.08373669, 0.99776368,
+        0.83234612, 0.03677736
+    };
+    size_t dimension = 2;
+    GEO::NearestNeighborSearch_var interp = GEO::NearestNeighborSearch::create(dimension, "BNN");
+    interp->set_points(coords.size() / dimension, coords.data());
 
-    GEO::NearestNeighborSearch_var interp = GEO::NearestNeighborSearch::create(2, "BNN");
-    interp->set_points(coords.size() / 2, coords.data());
-    size_t found_index = interp->get_nearest_neighbor(query.data());
-    std::cout << "Index found with geogram: " << found_index << std::endl;
+    std::vector<size_t> indices_found;
+    size_t isize = query.size() / dimension;
+    indices_found.reserve(isize);
+    std::span<const double> myspan(query);
+
+    for (size_t i = 0; i < query.size(); i += dimension) {
+        auto subspan = myspan.subspan(i, dimension);
+        indices_found.push_back(
+            interp->get_nearest_neighbor(subspan.data())
+        );
+    }
+    for (auto &&i : indices_found) {
+        std::cout << "Index found with geogram: " << i << std::endl;
+    }
 
     return 0;
 }
--- minimal_example.py
+++ minimal_example.py
@@ -1,18 +1,20 @@
 import numpy as np
 import scipy.interpolate as si
+import deli as dm
 
 coords = np.array([
-    # [.1, 0.8], # NOTE Uncommented, changes answer below to index 0. Commented out, answer is 4
-    [0.63696169, 0.26978671],
-    [0.04097352, 0.01652764],
-    [0.81327024, 0.91275558],
-    [0.60663578, 0.72949656],
-    [0.54362499, 0.93507242]
+    [0.84507479, 0.16097309],
+    [0.55774455, 0.36807994],
+    [0.21494196, 0.38582404],
+    [0.4281645,  0.61134311]
 ])
 values = np.array([
-    # 0.5, # NOTE toggle on to be consistent with above
-    0.81585355, 0.0027385, 0.85740428, 0.03358558, 0.72965545])
-query = np.array([0.17565562, 0.86317892])
+    0.73638587, 0.01528966, 0.25404091, 0.60414585
+])
+query = np.array([
+    [0.08373669, 0.99776368],
+    [0.83234612, 0.03677736]
+])
 
 pyinterp = si.NearestNDInterpolator(coords, values)
 queried_value = pyinterp(query)
@@ -20,9 +22,23 @@ queried_value = pyinterp(query)
 nn_distances, queried_index = pyinterp.tree.query(query, k=1)
 queried_value_2 = values[queried_index]
 
-my_distances = np.linalg.norm(query - coords, axis=1)
+my_indices = np.asarray([np.argmin(np.linalg.norm(q - coords, axis=1)) for q in query])
+my_distances = np.asarray([np.min(np.linalg.norm(q - coords, axis=1)) for q in query])
 
 np.testing.assert_equal(queried_value, queried_value_2)
-np.testing.assert_equal(np.argmin(my_distances), queried_index)
+np.testing.assert_equal(my_indices, queried_index)
 
-print(f"Index found by Python: {queried_index}")
+print(f"Python Indices: {queried_index}")
+print(f"Python neighbors: {queried_value}")
+
+dim=2
+cinterp = dm.NNInterpolator(dim, coords.flatten(), values)
+orig_coords = cinterp.get_coords()
+print(orig_coords)
+cindices = np.asarray(cinterp.find_nn(query.flatten()))
+cnewvals2 = values[cindices]
+cnewvals = np.asarray(cinterp.get_values(cindices))
+np.testing.assert_equal(queried_index, cindices)
+np.testing.assert_equal(cnewvals, cnewvals2)
+print(f"C++ Indices: {cindices}")
+print(f"C++ neighbors: {cnewvals}")
